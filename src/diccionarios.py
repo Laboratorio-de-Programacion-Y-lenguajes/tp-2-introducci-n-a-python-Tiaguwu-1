bdba580c93a6c10cd076dad2cf9ebf2147ffb2ce
Tiaguwu <tiagocaranchi@hotmail.com>
2026-04-07 22:40:13 -0300

    Agrego funciones a diccionarios.py

--- src/diccionarios.py
+++ src/diccionarios.py
@@ -9,7 +9,14 @@ def contar_palabras(texto: str) -> dict:
     Ejemplo: contar_palabras("hola mundo hola") -> {"hola": 2, "mundo": 1}
     Las palabras deben ser comparadas en minúsculas.
     """
-    # TU CÓDIGO AQUÍ
+    lista = {}
+    for palabra in texto.split():
+        palabra = palabra.lower()
+        if palabra in lista:
+            lista[palabra] += 1
+        else:
+            lista[palabra] = 1
+    return lista
     pass
 
 
@@ -18,7 +25,10 @@ def invertir_diccionario(d: dict) -> dict:
     Retorna un nuevo diccionario con claves y valores intercambiados.
     Ejemplo: invertir_diccionario({"a": 1}) -> {1: "a"}
     """
-    # TU CÓDIGO AQUÍ
+    invertido = {}
+    for clave, valor in d.items():
+        invertido [valor] = clave
+    return invertido
     pass
 
 
@@ -26,7 +36,9 @@ def merge_diccionarios(d1: dict, d2: dict) -> dict:
     """
     Combina dos diccionarios. Si hay claves repetidas, prevalece d2.
     """
-    # TU CÓDIGO AQUÍ
+    union = d1.copy()
+    union.update(d2)
+    return union
     pass
 
 
@@ -35,5 +47,9 @@ def filtrar_por_valor(d: dict, minimo: int) -> dict:
     Retorna un nuevo diccionario con solo los pares
     cuyo valor sea >= minimo.
     """
-    # TU CÓDIGO AQUÍ
+    pares = {}
+    for clave, valor in d.items():
+        if valor >= minimo:
+            pares[clave] = valor
+    return pares
     pass

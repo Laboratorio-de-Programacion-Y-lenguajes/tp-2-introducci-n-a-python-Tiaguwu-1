f56d32b70992680c3c048b816f8cfc8f87381327
Tiaguwu <tiagocaranchi@hotmail.com>
2026-04-07 23:58:28 -0300

    Agrego operaciones.py

--- src/operaciones.py
+++ src/operaciones.py
@@ -8,7 +8,9 @@ def es_palindromo(texto: str) -> bool:
     Retorna True si el texto es palíndromo (ignorando espacios y mayúsculas).
     Ejemplo: es_palindromo("Anita lava la tina") -> True
     """
-    # TU CÓDIGO AQUÍ
+    texto_limpio = texto.lower().replace(" ","")
+    invertido = texto_limpio[::-1]
+    return invertido == texto_limpio
     pass
 
 
@@ -17,7 +19,10 @@ def capitalizar_palabras(texto: str) -> str:
     Capitaliza la primera letra de cada palabra.
     Ejemplo: capitalizar_palabras("hola mundo") -> "Hola Mundo"
     """
-    # TU CÓDIGO AQUÍ
+    capitalizada = ""
+    for palabra in texto.split():
+        capitalizada += palabra.capitalize() + " "
+    return capitalizada.strip()
     pass
 
 
@@ -26,7 +31,13 @@ def contar_vocales(texto: str) -> int:
     Retorna la cantidad de vocales (a,e,i,o,u) en el texto,
     sin distinguir mayúsculas/minúsculas.
     """
-    # TU CÓDIGO AQUÍ
+    contador = 0
+    vocales = "aeiou"
+
+    for caracter in texto.lower():
+        if caracter in vocales:
+            contador += 1
+    return contador
     pass
 
 
@@ -36,5 +47,20 @@ def caesar_cipher(texto: str, desplazamiento: int) -> str:
     Solo desplaza letras (a-z, A-Z), los demás caracteres no cambian.
     Ejemplo: caesar_cipher("abc", 1) -> "bcd"
     """
-    # TU CÓDIGO AQUÍ
+def caesar_cipher(texto: str, desplazamiento: int) -> str:
+    resultado = ""
+    
+    for caracter in texto:
+        if 'A' <= caracter <= 'Z':
+            nuevo_codigo = (ord(caracter) - ord('A') + desplazamiento) % 26
+            resultado += chr(nuevo_codigo + ord('A'))
+            
+        elif 'a' <= caracter <= 'z':
+            nuevo_codigo = (ord(caracter) - ord('a') + desplazamiento) % 26
+            resultado += chr(nuevo_codigo + ord('a'))
+            
+        else:
+            resultado += caracter
+            
+    return resultado
     pass

2cf525804663ce78c85f73eff9b1f04376ca27b7
Tiaguwu <tiagocaranchi@hotmail.com>
2026-04-07 21:08:23 -0300

    Agrego funciones variables.py

--- src/variables.py
+++ src/variables.py
@@ -7,44 +7,33 @@
 
 
 def crear_saludo(nombre: str) -> str:
-    """
-    Retorna un saludo personalizado.
-    Ejemplo: crear_saludo("Ana") -> "Hola, Ana!"
-    """
-    # TU CÓDIGO AQUÍ
+    return f"Hola, {nombre}!"
     pass
 
 
 def suma_enteros(a: int, b: int) -> int:
-    """
-    Retorna la suma de dos enteros.
-    """
-    # TU CÓDIGO AQUÍ
+    return a + b
     pass
 
 
 def es_mayor_de_edad(edad: int) -> bool:
-    """
-    Retorna True si edad >= 18, False caso contrario.
-    """
-    # TU CÓDIGO AQUÍ
+    return edad >=18
     pass
 
 
 def tipo_de_dato(valor) -> str:
-    """
-    Retorna el nombre del tipo de dato del valor recibido.
-    Ejemplo: tipo_de_dato(42) -> "int"
-             tipo_de_dato("hola") -> "str"
-    """
-    # TU CÓDIGO AQUÍ
+    match (valor):
+        case bool():
+            return "bool"
+        case int():
+            return "int"
+        case str():
+            return "str"
+        case float():
+            return "float"
     pass
 
 
 def convertir_a_float(valor: str) -> float:
-    """
-    Convierte un string numérico a float.
-    Ejemplo: convertir_a_float("3.14") -> 3.14
-    """
-    # TU CÓDIGO AQUÍ
+    return float(valor)
     pass
